778d48cb57b2999bc2f92d8925f54ef8f145cbcb
Ekwusie Sophia <sophiaekwusie70@gmail.com>
2024-08-26 19:36:32 +0100

    Day one

---
+++ Day1_Band_generator.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+print('Welcome to Band Name Generator!')
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+band_name = city + pet
+print('Your Band Name is ', band_name)
