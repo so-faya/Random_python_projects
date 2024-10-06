3b52c494454e9c5b050c48d809099f29d09d496b
so-faya <sophiaekwusie70@gmail.com>
2024-10-06 04:20:37 +0100

    Day8_update

--- Day8_cipher_caesar.py
+++ Day8_cipher_caesar.py
@@ -1,6 +1,6 @@
 import art
 
-print(art.logo)
+print(Day8art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
@@ -27,6 +27,17 @@ should_continue = True
 while should_continue:
 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    direction_choice = True
+    while direction_choice:
+        if direction == "encode":
+            direction_choice= False
+        elif direction == "decode":
+            direction_choice = False
+        else:
+            print("Wrong command. Try again")
+            should_continue = True
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+            
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
